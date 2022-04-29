d0d28a4b7b7467c33d56f0454bdf68523966389b
MarcusAdams1220 <marcusadams1220@gmail.com>
2022-04-29 14:57:49 +1000

    Images of my plan

---
+++ main.js
@@ -0,0 +1,32 @@
+// Start the game on player 1's turn 
+    // Display that's it's player 1's turn
+    // Allow player 1 to click any game square
+    // Add "X" to the clicked square
+    // Disable the clicked square from being clicked again
+    // Check if player 1 wins
+
+// If player 1 wins...
+    // Display "Player 1 Wins!"
+    // Disable all game squares from being clicked
+
+
+// If player 1 does NOT win, make it player 2's turn
+    // Display that's it's player 2's turn
+    // Allow player 2 to click any game square
+    // Add "O" to the clicked square
+    // Disable the clicked square from being clicked again
+    // Check if player 2 wins
+
+
+// If player 2 wins... 
+    // Display "Player 2 Wins!"
+    // Disable all game squares from being clicked
+
+
+// If player 2 does NOT win, make it player 1's turn
+
+// Create a rematch button
+// If either player clicks the rematch button at any time...
+    // Clear all the X's & O's from all the game squares.
+    // Allow all game squares to be clicked
+    // Make it player 1's turn
